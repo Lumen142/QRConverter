2e628fe8e656716f55f135eec711ccb66b80a36f
Lumen142 <l8100032@gmail.com>
2025-08-19 02:25:19 +0300

    ClientSide, comment lines added.

--- public/script.js
+++ public/script.js
@@ -1,14 +1,21 @@
-const socket = io("http://localhost:3000/"); 
-console.log("client başladı.");
+// Variables
 
 let img = document.getElementById("qrimg")
 
 let submit = document.getElementById("sub1")
 let textbox = document.getElementById("textbox")
 
+// SocketIO : socket
+
+const socket = io("http://localhost:3000/");
+
+// Events
+
 submit.addEventListener("click", () => {
     socket.emit("qr", textbox.value)
 })
+
+// SocketIO : Connect server & Listener
 
 socket.on("connect", () => {
     console.log("bağlantı kuruldu.");
